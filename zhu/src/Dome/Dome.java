23b31f1c62934e6bed926e4868a3ab4a51bbc671
zhupeicheng <1984046257@qq.com>
2018-01-02 17:06:17 +0800

    Hellowork

---
+++ zhu/src/Dome/Dome.java
@@ -0,0 +1,17 @@
+package Dome;
+
+public class Dome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("是的范德萨发");
+     int a;
+     int b;
+     int c;
+     a=1;
+     b=a;
+     c=b+1;
+     System.out.println(a+b+c);
+	}
+
+}
---
+++ zhu/src/Dome2/Dome2.java
@@ -0,0 +1,30 @@
+package Dome2;
+
+import java.util.Scanner;
+
+public class Dome2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+   System.out.println("请输入一个数"); 
+   
+ 
+	
+	
+	int i=sc.nextInt();
+	
+	
+	for(int i1=0;i1<=8;i1++){
+		
+		
+	
+		System.out.println(i1); 
+		
+		
+	}	
+	
+}
+	
+
+}
